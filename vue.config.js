38fe0db95d004f6ee30846fb4302d24b7a78e452
Nicholas Wilson <nicholaswilson@Nicholass-Air.domain>
2019-03-12 11:57:18 -0500

    Changed to remote devServer

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     devServer: {
       proxy: {
         "/api/reservations-application/*": {
-          // target: "https://csci4830.outin.space/",
-          target: "http://localhost:8083/",
+          target: "https://csci4830.outin.space/",
+          // target: "http://localhost:8083/",
           secure: true
         }
       }
